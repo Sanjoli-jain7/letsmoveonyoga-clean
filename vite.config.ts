752a7dc81259b5cdd73542427144ce92f2e6c403
Sanjoli-jain7 <sanjolij375@gmail.com>
2026-07-17 12:41:19 +0530

    Fix Vite multi-page build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        classes: resolve(__dirname, "classes.html"),
+        contact: resolve(__dirname, "contact.html"),
+        faq: resolve(__dirname, "faq.html"),
+        gallery: resolve(__dirname, "gallery.html"),
+        location: resolve(__dirname, "location.html"),
+        schedule: resolve(__dirname, "schedule.html"),
+        testimonials: resolve(__dirname, "testimonials.html"),
+        thankyou: resolve(__dirname, "thankyou.html"),
+        videos: resolve(__dirname, "videos.html"),
+        workshops: resolve(__dirname, "workshops.html"),
+      },
+    },
+  },
   server: {
     port: 5173,
-    open: true
-  }
+    open: true,
+  },
 });
